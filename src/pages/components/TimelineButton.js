73982bf1c5037a40efbbcf302c38987ca923fd30
CertaintyItIs <itaungui@gmail.com>
2024-11-01 13:02:28 +0800

    updated passing of props for timelinetable

--- src/pages/components/TimelineButton.js
+++ src/pages/components/TimelineButton.js
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from 'react';
 import { createPortal } from 'react-dom';
 import TimelineTable from './timelinetable.js';
